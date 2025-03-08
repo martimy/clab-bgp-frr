b72d95cfd41675816c40cbdbcc13eac81a666e86
Maen Artimy <artimym@gmail.com>
2025-03-08 11:14:19 -0400

    update router names

--- router_connect_test.sh
+++ router_connect_test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Test host-leaf connectivity
-docker exec clab-bgp_frr-leaf1 ping 192.168.11.2 -c 1
-docker exec clab-bgp_frr-leaf1 ping 192.168.12.2 -c 1
-docker exec clab-bgp_frr-leaf2 ping 192.168.21.2 -c 1
-docker exec clab-bgp_frr-leaf2 ping 192.168.22.2 -c 1
-docker exec clab-bgp_frr-leaf3 ping 192.168.31.2 -c 1
-docker exec clab-bgp_frr-leaf3 ping 192.168.32.2 -c 1
+docker exec clab-fdc-leaf01 ping clab-fdc-spine01 -c 1
+docker exec clab-fdc-leaf01 ping clab-fdc-spine02 -c 1
+docker exec clab-fdc-leaf02 ping clab-fdc-spine01 -c 1
+docker exec clab-fdc-leaf02 ping clab-fdc-spine02 -c 1
+docker exec clab-fdc-leaf03 ping clab-fdc-spine01 -c 1
+docker exec clab-fdc-leaf03 ping clab-fdc-spine02 -c 1
 
